990780c64aa90e8343a560c1d32a5a60676bb7f4
alejandro <alexro2404@hotmail.com>
2015-07-17 14:46:38 -0500

    Preparando el proyecto

---
+++ src/codeqr/CodeQR.java
@@ -0,0 +1,15 @@
+package codeqr;
+
+/**
+ *
+ * @author root
+ */
+public class CodeQR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
